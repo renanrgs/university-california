a74e26b9d27b916756000a7dd16ba557f34ffd97
renan <renan.rgsilva@gmail.com>
2017-12-24 20:28:22 -0200

    Palindrome solution through for loop

--- Palindrome.java
+++ Palindrome.java
@@ -4,7 +4,7 @@ public class Palindrome {
 	public static void main(String... args){
 		Scanner scanner = new Scanner(System.in);
 		String word = scanner.next();
-		System.out.println(isPalindrome(word));
+		System.out.println(isPalindromeFor(word));
 	}
 
 	private static boolean isPalindrome(String word){
@@ -12,7 +12,15 @@ public class Palindrome {
 		if(word.charAt(0) != word.charAt(word.length() - 1)) return false;
 		return isPalindrome(word.substring(1, word.length() - 1));
 	}
+
+	private static boolean isPalindromeFor(String word){
+		char[] wordArray = word.toCharArray();
+		for(int i = 0, j = wordArray.length - 1; i < wordArray.length; i++, j--){
+			if(wordArray[i] != wordArray[j]){
+				return false;	
+			}
+		}
+
+		return true;
+	}
 }
-/*
-	ana	
-*/
