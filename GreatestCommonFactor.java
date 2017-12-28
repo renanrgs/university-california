8452f11e0c73f31739c8a1b945dbbec2f5df3e1e
renanrgs <renan.rgsilva@gmail.com>
2017-12-28 16:17:14 -0200

    Greatest Common Factor recursive solution

---
+++ GreatestCommonFactor.java
@@ -0,0 +1,13 @@
+public class GreatestCommonFactor{
+	public static void main(String[] args){
+		System.out.println(calculate(20, 4));
+	}
+
+	private static int calculate(int a, int b){
+		if(b == 0) return a;
+		return calculate(b, a % b);
+	}	
+}
+/*
+
+*/
