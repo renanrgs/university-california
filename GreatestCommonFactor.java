f35e87e2b63e95af41f8dd0d3c4b24abab62f387
renanrgs <renan.rgsilva@gmail.com>
2017-12-28 16:24:12 -0200

    Euclidean Algorithm

--- GreatestCommonFactor.java
+++ GreatestCommonFactor.java
@@ -3,6 +3,9 @@ public class GreatestCommonFactor{
 		System.out.println(calculate(20, 4));
 	}
 
+	/**
+		Euclidean solution
+	**/
 	private static int calculate(int a, int b){
 		if(b == 0) return a;
 		return calculate(b, a % b);
