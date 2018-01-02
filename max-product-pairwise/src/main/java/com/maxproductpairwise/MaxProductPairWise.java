dd4643dae807cd0142fd6c95f2c8a8532c337f0e
renanrgs <renan.rgsilva@gmail.com>
2018-01-02 17:20:46 -0200

    Improvement. Clean code

--- max-product-pairwise/src/main/java/com/maxproductpairwise/MaxProductPairWise.java
+++ max-product-pairwise/src/main/java/com/maxproductpairwise/MaxProductPairWise.java
@@ -1,6 +1,33 @@
 package com.maxproductpairwise;
 
 public class MaxProductPairWise {
+	public long getFasterSolution(long[] input) {
+		return Long.max(getHighestNegativeProduct(input), getHighestPositiveProduct(input));
+	}
+
+	private long getHighestPositiveProduct(long[] input) {
+		long positiveProduct = input[0] * input[1];
+		long highestElement = Long.max(input[0], input[1]);
+		for (int i = 2; i < input.length; i++) {
+			if (positiveProduct < highestElement * input[i]) {
+				positiveProduct = highestElement * input[i];
+			}
+			highestElement = Long.max(highestElement, input[i]);
+		}
+		return positiveProduct;
+	}
+
+	private long getHighestNegativeProduct(long[] input) {
+		long negativeProduct = input[0] * input[1];
+		long lowestElement = Long.min(input[0], input[1]);
+		for (int i = 2; i < input.length; i++) {
+			if (negativeProduct < lowestElement * input[i]) {
+				negativeProduct = lowestElement * input[i];
+			}
+			lowestElement = Long.min(lowestElement, input[i]);
+		}
+		return negativeProduct;
+	}
 
 	public long getSlowerSolution(long[] array) {
 		long result = 0;
@@ -14,55 +41,29 @@ public class MaxProductPairWise {
 		}
 		return result;
 	}
-	//
-	// public long getFasterSolution(long[] array) {
-	//
-	// int maxIndex = -1;
-	// for (int i = 0; i < array.length; i++) {
-	// if (maxIndex == -1 || array[i] > array[maxIndex]) {
-	// maxIndex = i;
-	// }
-	// }
-	//
-	// int maxIndex2 = -1;
-	// for (int i = 0; i < array.length; i++) {
-	// if (i == maxIndex) {
-	// continue;
-	// }
-	//
-	// if (maxIndex2 == -1 || array[i] > array[maxIndex2]) {
-	// maxIndex2 = i;
-	// }
-	// }
-	//
-	// return array[maxIndex] * array[maxIndex2];
-	// }
 
-	public long getFasterSolution(long[] input) {
-		long positiveProduct = input[0] * input[1];
-		long highestElement = Long.max(input[0], input[1]);
+	@Deprecated
+	public long getFasterSolution(int[] array) {
 
-		long negativeProduct = input[0] * input[1];
-		long lowestElement = Long.min(input[0], input[1]);
-
-		for (int i = 2; i < input.length; i++) {
-			if (positiveProduct < highestElement * input[i]) {
-				positiveProduct = highestElement * input[i];
+		int maxIndex = -1;
+		for (int i = 0; i < array.length; i++) {
+			if (maxIndex == -1 || array[i] > array[maxIndex]) {
+				maxIndex = i;
 			}
-			highestElement = Long.max(highestElement, input[i]);
-
-			if (negativeProduct < lowestElement * input[i]) {
-				negativeProduct = lowestElement * input[i];
-			}
-
-			lowestElement = Long.min(lowestElement, input[i]);
 		}
-		return positiveProduct > negativeProduct ? positiveProduct : negativeProduct;
+
+		int maxIndex2 = -1;
+		for (int i = 0; i < array.length; i++) {
+			if (i == maxIndex) {
+				continue;
+			}
+
+			if (maxIndex2 == -1 || array[i] > array[maxIndex2]) {
+				maxIndex2 = i;
+			}
+		}
+
+		return array[maxIndex] * array[maxIndex2];
 	}
 
 }
-/*
- * 3, 6, 4, 2, 7
- * 
- * 3 * 6 >> 6 * 4 >> 6 * 2 >> 6 * 7
- */
